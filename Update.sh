f8be03a3e33f90cf0a7622ec596779fc61ab4cec
Michael <dartvauder007@protonmail.com>
2024-05-29 19:24:07 +0300

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -9,6 +9,7 @@ source "$CURRENT_DIR/venv/bin/activate"
 echo "Updating dependencies.../Обновление зависимостей..."
 pip install --no-deps -r "$CURRENT_DIR/requirements.txt"
 pip install --no-deps -r "$CURRENT_DIR/requirements-cuda.txt"
+pip install --no-deps -r "$CURRENT_DIR/requirements-llama-cpp.txt"
 clear
 
 echo "Application has been updated successfully. Run start.sh/Приложение успешно обновлено. Запустите start.sh"

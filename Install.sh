a9e54412c576d0e19a8884dd8e13be3b94d986d6
Michael <dartvauder007@protonmail.com>
2024-09-18 23:15:09 +0300

    RETURN-LIL-UPDATE

--- Install.sh
+++ Install.sh
@@ -16,6 +16,7 @@ echo "Installing dependencies.../Установка зависимостей..."
 pip install --no-deps -r "$CURRENT_DIR/requirements.txt"
 pip install --no-deps -r "$CURRENT_DIR/requirements-cuda.txt"
 pip install --no-deps -r "$CURRENT_DIR/requirements-llama-cpp.txt"
+pip install --no-deps -r "$CURRENT_DIR/requirements-stable-diffusion-cpp.txt"
 clear
 
 echo "Application has been installed successfully. Run start.sh/Приложение успешно установлено. Запустите start.sh"
